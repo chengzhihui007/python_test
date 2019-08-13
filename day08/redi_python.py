008bed76dc421eea886b98c75da0d89b5ff171ca
chengzhihui007 <469870113@qq.com>
2019-08-13 17:51:43 +0800

    desc: 爬虫完结。

--- day08/redi_python.py
+++ day08/redi_python.py
@@ -2,11 +2,12 @@
 import redis
 
 #1.连接数据库 key-value
-client = redis.StrictRedis(host='127.0.0.1',port=6379)
+client = redis.StrictRedis(host='192.168.124.132',port=6379)
 
 #2,设置key
 key = 'pyone'
 #3,string 增加
+client.incr('aaa')
 result = client.set(key,"1")
 #4.删 成功1 失败0
 result = client.delete(key)
@@ -18,5 +19,5 @@ print(result)
 result = client.get(key)
 
 #查看所有的建
-result = client.keys('one')
+result = client.keys('*')
 print(result)
